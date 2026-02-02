d397b8be1fb2c377c7d3244a2f3d092c6e66e82e
k3g3l-pixel <k3g3l@tlu.ee>
2026-02-02 13:41:09 +0000

    midagi-esmaspaev

---
+++ t_test1.py
@@ -0,0 +1,4 @@
+from scipy.stats import ttest_ind
+print(ttest_ind([3, 5, 4], [12, 16, 14]))
+print(ttest_ind([13, 15, 14], [12, 16, 14]))
+
---
+++ t_test2.py
@@ -0,0 +1,19 @@
+lause1="Kui Arno isaga koolimajja jõudis olid tunnid juba alanud"
+lause2="Mu isamaa mu õnn ja rõõm kui kaunis oled sa"
+sonad1=lause1.split( )
+print(sonad1)
+print(sonad1[3])
+print(len(sonad1[3]))
+print(len(sonad1))
+sonapikkused1=[len(sona) for sona in sonad1]
+print(sonapikkused1)
+
+sonad2=lause2.split( )
+print(sonad2)
+print(sonad2[3])
+print(len(sonad2[3]))
+print(len(sonad2))
+sonapikkused2=[len(sona) for sona in sonad2]
+print(sonapikkused2)
+#print(ttest_ind(sonapikkused1, sonapikkused2))
+print(sum(sonapikkused1/len(sonapikkused1)))
